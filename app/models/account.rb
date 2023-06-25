b4e0ae379b207b3ad0ba09ab6257471c2c1f1da4
nachtjasmin <nachtjasmin@posteo.de>
2023-06-25 17:51:48 +0200

    Increase the character limit of biographies to 1000 chars

--- app/models/account.rb
+++ app/models/account.rb
@@ -91,7 +91,7 @@ class Account < ApplicationRecord
   validates :username, format: { with: /\A[a-z0-9_]+\z/i }, length: { maximum: 30 }, if: -> { local? && will_save_change_to_username? && actor_type != 'Application' }
   validates_with UnreservedUsernameValidator, if: -> { local? && will_save_change_to_username? && actor_type != 'Application' }
   validates :display_name, length: { maximum: 30 }, if: -> { local? && will_save_change_to_display_name? }
-  validates :note, note_length: { maximum: 500 }, if: -> { local? && will_save_change_to_note? }
+  validates :note, note_length: { maximum: 1000 }, if: -> { local? && will_save_change_to_note? }
   validates :fields, length: { maximum: 4 }, if: -> { local? && will_save_change_to_fields? }
 
   scope :remote, -> { where.not(domain: nil) }
